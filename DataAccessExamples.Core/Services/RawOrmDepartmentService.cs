ba111c0d8d7568bc689c3275b45286bd992d8052
Harry Cummings <harry.cummings@softwire.com>
2015-05-04 20:51:47 +0100

    Add Raw SQL via ORM implementation

---
+++ DataAccessExamples.Core/Services/RawOrmDepartmentService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessExamples.Core.Data;
+using DataAccessExamples.Core.ViewModels;
+
+namespace DataAccessExamples.Core.Services
+{
+    public class RawOrmDepartmentService : IDepartmentService
+    {
+        private EmployeesContext context;
+
+        public RawOrmDepartmentService(EmployeesContext context)
+        {
+            this.context = context;
+        }
+
+        public DepartmentList ListDepartments()
+        {
+            return new DepartmentList
+            {
+                Departments = context.Database.SqlQuery<DepartmentSummary>("SELECT Code, Name FROM Department ORDER BY Department.Code")
+            };
+        }
+
+        public DepartmentList ListAverageSalaryPerDepartment()
+        {
+            return new DepartmentList
+            {
+                Departments = context.Database.SqlQuery<DepartmentSalary>(@"
+SELECT Department.Code, Department.Name, CAST(AVG(CAST(Amount AS bigint)) AS int) AS 'AverageSalary'
+FROM Department
+JOIN DepartmentEmployee ON Department.Code = DepartmentCode
+JOIN Salary ON DepartmentEmployee.EmployeeNumber = Salary.EmployeeNumber
+WHERE Salary.ToDate > GETDATE()
+GROUP BY Department.Code, Department.Name
+ORDER BY AverageSalary DESC")
+            };
+        }
+    }
+}
