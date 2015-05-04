d3f40a00bb156bcfea86d7be0e562cb2f73eab8b
Harry Cummings <harry.cummings@softwire.com>
2015-05-04 19:02:36 +0100

    Add Eager ORM implementation

---
+++ DataAccessExamples.Core/Services/EagerOrmDepartmentService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using DataAccessExamples.Core.Data;
+using DataAccessExamples.Core.ViewModels;
+
+namespace DataAccessExamples.Core.Services
+{
+    public class EagerOrmDepartmentService : IDepartmentService
+    {
+        private readonly EmployeesContext context;
+
+        public EagerOrmDepartmentService(EmployeesContext context)
+        {
+            this.context = context;
+        }
+
+        public DepartmentList ListDepartments()
+        {
+            return new DepartmentList {
+                Departments = context.Departments.OrderBy(d => d.Code).Select(d => new DepartmentSummary
+                {
+                    Code = d.Code,
+                    Name = d.Name
+                })
+            };
+        }
+
+        public DepartmentList ListAverageSalaryPerDepartment()
+        {
+            return new DepartmentList
+            {
+                Departments = context.Departments.AsEnumerable().Select(d => new DepartmentSalary
+                {
+                    Code = d.Code,
+                    Name = d.Name,
+                    AverageSalary =
+                        (int) d.DepartmentEmployees.Select(
+                            e => e.Employee.Salaries.LastOrDefault(s => s.ToDate > DateTime.Now))
+                            .Where(s => s != null)
+                            .Average(s => s.Amount)
+                })
+            };
+        }
+    }
+}
