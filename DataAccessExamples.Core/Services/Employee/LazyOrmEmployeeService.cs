b8e24e93bb189eac7f4fc58d7e90512c4ad69720
Harry Cummings <harry.cummings@softwire.com>
2015-05-13 11:15:16 +0100

    Add (intentionally) broken Lazy ORM Employee service

--- DataAccessExamples.Core/Services/Employee/LazyOrmEmployeeService.cs
+++ DataAccessExamples.Core/Services/Employee/LazyOrmEmployeeService.cs
@@ -1,23 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DataAccessExamples.Core.Actions;
+using DataAccessExamples.Core.Data;
 using DataAccessExamples.Core.ViewModels;
 
 namespace DataAccessExamples.Core.Services.Employee
 {
     public class LazyOrmEmployeeService : IEmployeeService
     {
+        private readonly EmployeesContext context;
+
+        public LazyOrmEmployeeService(EmployeesContext context)
+        {
+            this.context = context;
+        }
+
         public void AddEmployee(AddEmployee action)
         {
-            throw new NotImplementedException();
+            var employee = Mapper.Map<Data.Employee>(action);
+            employee.DepartmentEmployees.Add(new DepartmentEmployee
+            {
+                Employee = employee,
+                DepartmentCode = action.DepartmentCode,
+                FromDate = action.HireDate,
+                ToDate = DateTime.MaxValue
+            });
+            context.Employees.Add(employee);
+            context.SaveChanges();
         }
 
         public EmployeeList ListRecentHires()
         {
-            throw new NotImplementedException();
+            return new EmployeeList
+            {
+                Employees = context.Employees
+                    .Where(e => e.HireDate > DateTime.Now.AddDays(-7))
+                    .OrderByDescending(e => e.HireDate)
+            };
         }
     }
 }
